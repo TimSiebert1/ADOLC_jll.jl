4d35317f80eb59943ffec414ebd71bb46b1f2d0e
TimSiebert1 <tim.siebert96@outlook.com>
2024-06-17 18:09:02 +0200

    ADOLC_jll build 1.1.1+0

--- src/wrappers/aarch64-apple-darwin-julia_version+1.9.0.jl
+++ src/wrappers/aarch64-apple-darwin-julia_version+1.9.0.jl
@@ -2,11 +2,10 @@
 export libadolc_wrap
 
 using libcxxwrap_julia_jll
-using libjulia_jll
 JLLWrappers.@generate_wrapper_header("ADOLC")
 JLLWrappers.@declare_library_product(libadolc_wrap, "@rpath/libadolc_wrap.0.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(libcxxwrap_julia_jll, libjulia_jll)
+    JLLWrappers.@generate_init_header(libcxxwrap_julia_jll)
     JLLWrappers.@init_library_product(
         libadolc_wrap,
         "lib/libadolc_wrap.0.0.1.dylib",
